1828cc00a8a79c51d787b6634d0b77563bcdd237
manasvislj <manasvisaluja2003@gmail.com>
2023-09-27 09:25:38 -0400

    New Daily challenge

---
+++ Morning_Exercises/ReverseUpperCaseString.js
@@ -0,0 +1,17 @@
+/*-----------------------------------------------------------------
+Challenge: 07-reverseUpcaseString
+Difficulty: Basic
+Prompt:
+- Write a function called reverseUpcaseString that accepts a single string argument, then returns the string with its characters in reverse order and converts all characters to uppercase.
+Examples:
+reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES" 
+-----------------------------------------------------------------*/
+// Your solution for 07-reverseUpcaseString here:
+
+function reverseUpcaseString(string){
+    for (let i=1; i <string.length-1; i--){
+        let string = newString;
+    }
+    return newString;
+}
+reverseUpcaseString("harry");
