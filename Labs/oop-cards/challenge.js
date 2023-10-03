da9fc81e3b5f014c4e02eae5889f993c4d63a703
manasvislj <manasvisaluja2003@gmail.com>
2023-10-03 14:08:25 -0400

    Added oop Card lab

---
+++ Labs/oop-cards/challenge.js
@@ -0,0 +1,80 @@
+/*
+ * Define a Card class with the following properties:
+ *
+ *   - suit (hearts, spades, clubs, diamonds)
+ *   - rank (Ace, 2, 3, 4, .. Jack, Queen, King)
+ *   - score (1, 2, 3, 4, ... 11, 12, 13)
+ *
+ * Define a Deck class with the following properties and methods:
+ *
+ *   - length (the number of cards - should start at 52)
+ *   - cards (an array of cards in the deck)
+ *   - draw: return a random card from the cards array
+ *
+ * When you create an instance of your Deck class (i.e. in your constructor),
+ * fill in the cards array with 52 instances of your Card class. You can do
+ * this with a nested for loop - first loop through an array of all possible
+ * suits, then loop through an array of all possible ranks. Inside your inner
+ * loop, create an instance of your Card class and push it into the Deck's cards
+ * array.
+ *
+ * Instantiate an instance of your Deck and start drawing random cards!
+ */
+
+class Card {
+  constructor(suit, rank, score) {
+    this.suit = suit;
+    this.rank = rank;
+    this.score = score;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    this.createDeck();
+  }
+
+  createDeck() {
+    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+    const ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace",
+    ];
+
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        const card = new Card(suits[i], ranks[j], j + 2);
+        this.cards.push(card);
+      }
+    }
+
+    this.shuffle();
+  }
+
+  shuffle() {
+    const cards = this.cards;
+
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // [cards[i], cards[j]] = [cards[j], cards[i]]
+      let temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+  }
+}
+
+const gameDeck = new Deck();
+console.log(gameDeck.cards);
