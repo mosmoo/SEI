e84b084a50c66c15ccf344b9e4e3b30b878eca78
manasvislj <manasvisaluja2003@gmail.com>
2023-10-04 19:39:11 -0400

    Practiced oop cards

--- Labs/oop-cards practice/script.js
+++ Labs/oop-cards practice/script.js
@@ -1,7 +1,52 @@
-class Cards {
-    constructor(suit,rank,score) {
-        this.suit= suit;
-        this.rank=rank;
-        this.score=score;
-    }
+class Card {
+  constructor(suit, rank, score) {
+    this.suit = suit;
+    this.rank = rank;
+    this.score = score;
+  }
 }
+class Deck {
+  constructor() {
+    this.cards = [];
+    this.createDeck();
+  }
+  createDeck() {
+    const suits = ["hearts", "diamonds", "spades", "clubs"];
+    const ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace",
+    ];
+    for (let i = 0; i < suits.length; i++){
+        for (let j = 0; j <ranks.length; j++){
+            const card = new Card(suits[i],ranks[j],j+2)
+            this.cards.push(card);
+        }
+    }
+    this.shuffle();
+  }
+  shuffle() {
+    const cards = this.cards;
+
+    for (let i = 0; i < cards.length; i++) {
+      const j = Math.floor(Math.random() * (i - 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]]
+      /*let temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;*/
+    }
+  }
+}
+
+const gameDeck = new Deck();
+console.log(gameDeck.cards);
--- Labs/oop-cards/challenge.js
+++ Labs/oop-cards/challenge.js
@@ -68,10 +68,10 @@ class Deck {
 
     for (let i = cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      // [cards[i], cards[j]] = [cards[j], cards[i]]
-      let temp = cards[i];
+      [cards[i], cards[j]] = [cards[j], cards[i]]
+      /*let temp = cards[i];
       cards[i] = cards[j];
-      cards[j] = temp;
+      cards[j] = temp;*/
     }
   }
 }
