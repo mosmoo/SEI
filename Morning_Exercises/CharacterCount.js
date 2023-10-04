333e672ae73f3cb675c78f3c7727b812621c1e08
manasvislj <manasvisaluja2003@gmail.com>
2023-10-04 09:25:23 -0400

    New Morning Exercise

--- Morning_Exercises/CharacterCount.js
+++ Morning_Exercises/CharacterCount.js
@@ -11,16 +11,3 @@ charCount('hello') //=> { h: 1, e: 1, l: 2, o: 1 }
 charCount('Today is fantastic!') //=> { T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 }
 -----------------------------------------------------------------*/
 // Your solution for 09-charCount here:
-
-function charCount(str) {
-    let result = {};
-    for (let i = 0; i < str.length; i++) {
-      let char = str.charAt(i);
-      if (result[char]) {
-        result[char]++;
-      } else {
-        result[char] = 1;
-      }
-    }
-    return result;
-  }
