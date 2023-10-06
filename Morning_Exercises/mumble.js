22015495ae91851f37ded9f5c54b538925b61bd0
manasvislj <manasvisaluja2003@gmail.com>
2023-10-06 10:21:57 -0400

    Morning exercise answer

---
+++ Morning_Exercises/mumble.js
@@ -0,0 +1,26 @@
+/*-----------------------------------------------------------------
+Challenge: 13-mumble
+Difficulty: Intermediate
+Prompt:
+- Write a function called mumble that accepts a single string argument.
+- The function should return a string that has each character repeated the number of times according to its position within the string arg.  In addition, each repeated section of characters should be separated by a hyphen (-).
+- Examples describe it best..
+Examples:
+mumble('X'); //=> 'X'
+mumble('abc'); //=> 'a-bb-ccc'
+mumble('121'); //=> '1-22-111'
+mumble('!A 2'); //=> '!-AA-   -2222'
+-----------------------------------------------------------------*/
+// Your solution for 13-mumble here:
+
+function mumble (str){
+    str = str.split('')
+    for (let i=0;i<str.length; i++){
+        const currentChar = str[i]
+        for (let j=0;j<i;j++){
+            str[i] += currentChar
+        }
+    }
+    return str.join('-');
+}
+mumble("hello");
