63a29667bcc50ec7fec13804d3402cd17884f2a3
manasvislj <manasvisaluja2003@gmail.com>
2023-11-27 13:34:31 -0500

    Added sandbox file

---
+++ Sandbox/test.py
@@ -0,0 +1,9 @@
+print("Welcome to the Iron Rattler! How tall are you (in feet)?")
+height = int(input())
+
+if height < 4:
+    print("Sorry, you'll fly out of your seat if we let you on.")
+elif height < 7:
+    print("All aboard!")
+else:
+    print("If you value your head, you should not get on this ride." )
